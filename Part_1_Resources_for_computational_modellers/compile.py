95451c6271c4369dc02a66a3105c6504b5feaadf
Padraig Gleeson <p.gleeson@gmail.com>
2017-06-08 16:59:54 +0100

    File to compile pptx files to pdf

---
+++ Part_1_Resources_for_computational_modellers/compile.py
@@ -0,0 +1,31 @@
+import os
+from subprocess import call
+
+from pdfrw import PdfReader, PdfWriter
+          
+sections = ['Introduction','1_Experimental_datasets', '2_Structured_data_from_literature', '3_Analysis_tools', '4_Simulation_environments', '5_Model_sharing', '6_Computing_infrastructure', '7_Open_source_initiatives']
+#sections = ['Introduction','1_Experimental_datasets', '2_Structured_data_from_literature']
+
+big_file = PdfWriter()
+
+for section in sections:
+
+    for f in os.listdir(section):
+        fpath = section+'/'+f
+        if os.path.isfile(fpath) and fpath.endswith('pptx'):
+            print("Incorporating: %s"%fpath)
+            call(["libreoffice", "--headless", "--invisible", "--convert-to", "pdf", fpath])
+            
+            pdf_file_name = f.replace('pptx','pdf')
+            pdf_file = PdfReader(pdf_file_name)
+            print("  Adding pages from %s"%pdf_file_name)
+            big_file.addpages(pdf_file.pages)
+        
+
+big_file.write('Part1.pdf')
+
+
+print("Done.")
+
+
+
