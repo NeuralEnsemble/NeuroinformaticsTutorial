044ee05358ed05600f91d9df11c185328a00c11b
Padraig Gleeson <padraig@Padraigs-MacBook-Pro.local>
2017-07-13 12:57:35 +0100

    Update for comp on Mac

--- Part_1_Resources_for_computational_modellers/compile.py
+++ Part_1_Resources_for_computational_modellers/compile.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from subprocess import call
 
 from pdfrw import PdfReader, PdfWriter
@@ -6,30 +7,37 @@ from pdfrw import PdfReader, PdfWriter
 sections = ['Introduction','1_Experimental_datasets', '2_Structured_data_from_literature', '3_Analysis_tools', '4_Simulation_environments', '5_Model_sharing', '6_Computing_infrastructure', '7_Open_source_initiatives', '8_Web_portals']
 #sections = ['Introduction','1_Experimental_datasets', '2_Structured_data_from_literature']
 
+only_sec = None
+
+if len(sys.argv) == 2:
+    only_sec = sys.argv[1]
+    print("Only doing sections starting with %s"%only_sec)
 
 for section in sections:
 
-    print("++++++++++++++++++++++++++++++++++\n+  Adding section: %s\n+"%section)
-    big_file = PdfWriter()
-    
-    files = os.listdir(section)
-    
-    files = sorted(files)
-    
-    for f in files:
-        fpath = section+'/'+f
-        if os.path.isfile(fpath) and fpath.endswith('pptx') and not f=='Template.pptx':
-            print("+   Incorporating: %s"%fpath)
-            call(["libreoffice", "--headless", "--invisible", "--convert-to", "pdf", fpath])
-            pdf_file_name = f.replace('pptx','pdf')
-            
-            pdf_file = PdfReader(pdf_file_name)
-            print("+     Adding pages from %s\n+"%pdf_file_name)
-            big_file.addpages(pdf_file.pages)
-            call(["mv", pdf_file_name, "temp"])
-        
+    if only_sec==None or section.startswith(only_sec):
+        print("++++++++++++++++++++++++++++++++++\n+  Adding section: %s\n+"%section)
+        big_file = PdfWriter()
 
-    big_file.write('Part1_%s.pdf'%section)
+        files = os.listdir(section)
+
+        files = sorted(files)
+
+        for f in files:
+            fpath = section+'/'+f
+            if os.path.isfile(fpath) and fpath.endswith('pptx') and not f=='Template.pptx':
+                print("+   Incorporating: %s"%fpath)
+                call(["/Applications/LibreOffice.app/Contents/MacOS/soffice", "--headless", "--invisible", "--convert-to", "pdf", fpath])
+                pdf_file_name = f.replace('pptx','pdf')
+
+                pdf_file = PdfReader(pdf_file_name)
+                print("+     Adding pages from %s\n+"%pdf_file_name)
+                big_file.addpages(pdf_file.pages)
+                if not 'ICG' in pdf_file_name:
+                    call(["mv", pdf_file_name, "temp"])
+
+
+        big_file.write('Part1_%s.pdf'%section)
 
 
 print("Done.")
